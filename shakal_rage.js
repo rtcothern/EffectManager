4055146da7ade3286c99d0a6eced38c7ff3862e8
Raymond Cothern <rtcothern@gmail.com>
2020-04-25 11:29:52 -0700

    Clean up defines, batch the update operations at the end for stability

--- shakal_rage.js
+++ shakal_rage.js
@@ -1,6 +1,6 @@
 let rager = game.user.character;
-let chatMsg = '';       
-let level = rager.data.data.details.level.value;
+let ragerData = rager.data.data;
+let level = ragerData.details.level.value;
 
 let toggleRage = function(toggle)
 {
@@ -11,40 +11,38 @@ let toggleRage = function(toggle)
 
 	let mult = toggle ? 1 : -1;
 
-	let hpMod = rager.data.data.attributes.hp.temp + mult*(rager.data.data.abilities.con.mod + level);
+	let hpMod = ragerData.attributes.hp.temp + mult*(ragerData.abilities.con.mod + level);
 	hpMod = (hpMod >= 0) ? hpMod : 0;
 	obj['data.attributes.hp.temp'] = hpMod;
 
 	report += 'New Temp HP: ' + hpMod + '<br>';
 
-    let resVal = 3+level+rager.data.data.abilities.con.mod;
+    let resVal = 3+level+ragerData.abilities.con.mod;
 	let numNewDR = 0;
 
 	let toggleResFn = function(type, name)
 	{
-		let resInd = rager.data.data.traits.dr.findIndex( trait => trait.type == type );
+		let resInd = ragerData.traits.dr.findIndex( trait => trait.type == type );
 		if ( resInd == -1 )
 		{
-			resInd = rager.data.data.traits.dr.length + (numNewDR++);
+			resInd = ragerData.traits.dr.length + (numNewDR++);
 			obj['data.traits.dr'][resInd] = { type: type, label: name, value: (mult*resVal).toString(), exceptions: ''};
 		}
 		else
 		{
-			let numberValue = +rager.data.data.traits.dr[resInd].value;
+			let numberValue = +ragerData.traits.dr[resInd].value;
 			obj['data.traits.dr'][resInd].value = (+numberValue + mult*resVal).toString();
 		} 
 		report += 'New ' + name + ' Resistance: ' + obj['data.traits.dr'][resInd].value + '<br>';
 	}
-	obj['data.traits.dr'] = [...rager.data.data.traits.dr];
+	obj['data.traits.dr'] = [...ragerData.traits.dr];
 
 	toggleResFn('piercing', 'Piercing');
 	toggleResFn('slashing', 'Slashing');
 
+	// Batch our update operations at the end so we only do them if we haven't hit errors
 	let updateOperations = [];
-
 	updateOperations.push([ rager, obj]);
-
-	rager.update(obj);
 
 	let bonusDamage;
 	if (level >= 15 )
@@ -64,23 +62,25 @@ let toggleRage = function(toggle)
 		let itObj = {}; 
 		if (it.type == 'weapon' && (it.data.data.range.value == 'melee' || it.data.data.range.value == 'reach') )
 		{
-			
-
 			bonusDamage = it.data.data.traits.value.includes('agile') ? Math.floor(bonusDamage/2) : bonusDamage;
 			itObj['data.bonusDamageDamage.value'] = it.data.data.bonusDamage.value + mult*bonusDamage; 
-			
-			it.update(itObj);
+			updateOperations.push([ it, itObj]);
 		}
 		else if(it.type == 'armor' && it.data.data.equipped.value == true)
 		{
 			itObj['data.armor.value'] = it.data.data.armor.value - mult;
-			it.update(itObj);
+			updateOperations.push([ it, itObj]);
 		}
+	}
+
+	for ( operation of updateOperations )
+	{
+		operations[0].update(operation[1]);
 	}
 
 	let rageClause = toggle ? ' (Rage bonus halved for Agile Weapons)' : '';
 	report += 'Bonus Damage: ' + bonusDamage + rageClause + '<br>';
-	report += 'New AC: ' + (rager.data.data.attributes.ac.value - mult).toString();
+	report += 'New AC: ' + (ragerData.attributes.ac.value - mult).toString();
 
 	let token = canvas.tokens.ownedTokens.find(t => t.actor.id === rager.id);
 	let statusEffectIndex = 12; //Placeholder until we can get a better image
@@ -88,6 +88,7 @@ let toggleRage = function(toggle)
 	return report;
 }
 
+let chatMsg = '';       
 let obj = {};
 let toggle = (rager.data.flags.raging == undefined || rager.data.flags.raging == false);
 if (toggle)
